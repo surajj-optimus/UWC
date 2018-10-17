7a2da0b9dfed250e6856de4f6062b1e120dceddf
surajj-optimus <suraj.jena@optimusinfo.com>
2018-10-16 22:04:15 -0700

    .Net Induction

---
+++ Assignments/GarageManagement/GarageManagement/Scripts/ajax-calls.js
@@ -0,0 +1,28 @@
+﻿// handles the click event, sends the query
+function getSuccessOutput() {
+	var car = {
+		name: $("#name").val(),
+		model: $("#model").val(),
+		number: $("#number").val()
+	}
+	$.ajax({
+		url: '/Home/CarSubmit',
+		type: 'post',
+		dataType: 'json',
+		data: JSON.stringify(car),
+		contentType: 'application/json',
+		success: function (response) {
+			if (response != null) {
+				alert("Name : " + response.name + ", Model : " + response.model + ", Number :" + response.number);
+			} else {
+				alert("Something went wrong");
+			}
+		},
+		failure: function (response) {
+			alert("Something went wrong");
+		},
+		error: function (response) {
+			alert("Something went wrong");
+		}
+	});
+}
---
+++ Assignments/GarageManagement/GarageManagement/Utility/GarageUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageManagement.Utility
+{
+	/// <summary>
+	/// GarageUtility is an entity that is used for the purpose of referencing the canstant values.
+	/// </summary>
+	public class GarageUtility
+	{
+		public static readonly string AddView = "Index";
+		public static readonly string ViewFolder = "Home";
+	}
+}
